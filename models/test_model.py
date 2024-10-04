e0272015344c6143c70bf035fbe0213f16fa63c9
Jasper van der Gronde <jasper.vander.gronde@ict.nl>
2024-10-04 20:49:56 +0200

    Fixed build and tests

--- models/test_model.py
+++ models/test_model.py
@@ -1,5 +1,13 @@
 
-class TestModel:
+class MyTestModel:
+    def __init__(self):
+        self.tests = []
 
-    def __init__(self) -> None:
-        pass
+    def add_test(self, test_name):
+        """Add a test to the model."""
+        self.tests.append(test_name)
+
+    def remove_test(self, test_name):
+        """Remove a test from the model."""
+        if test_name in self.tests:
+            self.tests.remove(test_name)
--- tests/test_test_model.py
+++ tests/test_test_model.py
@@ -1,13 +1,15 @@
 import pytest
-from models.test_model import TestModel
+from models.test_model import MyTestModel
+
 
 def test_add_test():
-    model = TestModel()
+    model = MyTestModel()
     model.add_test('Test 1')
     assert 'Test 1' in model.tests
 
+
 def test_remove_test():
-    model = TestModel()
+    model = MyTestModel()
     model.add_test('Test 1')
     model.remove_test('Test 1')
     assert 'Test 1' not in model.tests
